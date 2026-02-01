4dc324af6a51c63676102dbc772fc920780dfa97
alifgbrn <alifalgibran147@gmail.com>
2026-02-01 15:45:34 +0800

    chore: config the tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#F6F5E8', // paper beige
+      },
+    },
+  },
+  plugins: [],
+}
